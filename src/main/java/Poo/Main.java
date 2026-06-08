d32c038d5e7214399ea0c17adb146c772617ff8b
Yuri Coene <yuri01coene@gmail.com>
2026-06-08 08:29:19 -0400

    4°Questão bigDecimal

--- src/main/java/Poo/Main.java
+++ src/main/java/Poo/Main.java
@@ -1,11 +1,14 @@
 package Poo;
+
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args){
-        Integer a = 127, b = 127;
-        System.out.println(a == b);
-        Integer c = 200, d = 200;
-        System.out.println(c == d);
-        System.out.println(c.equals(d));
+        BigDecimal preco = new BigDecimal("49.90");
+        BigDecimal qtd = new BigDecimal("3");
+        BigDecimal desconto = new BigDecimal("0.95");
+        BigDecimal total = preco.multiply(qtd).multiply(desconto);
+        System.out.println("Total: R$ " + total);
     }
 }
 
