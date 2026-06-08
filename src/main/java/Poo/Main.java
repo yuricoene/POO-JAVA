d63ccf3b60febed9dbc4931e7210138021e17a52
Yuri Coene <yuri01coene@gmail.com>
2026-06-08 08:26:33 -0400

    3° questão Wrappers equals

--- src/main/java/Poo/Main.java
+++ src/main/java/Poo/Main.java
@@ -1,9 +1,11 @@
 package Poo;
 public class Main {
     public static void main(String[] args){
-        Integer X = null;
-        System.out.println(X);
-        System.out.println(X.getClass().getSimpleName());
+        Integer a = 127, b = 127;
+        System.out.println(a == b);
+        Integer c = 200, d = 200;
+        System.out.println(c == d);
+        System.out.println(c.equals(d));
     }
 }
 
