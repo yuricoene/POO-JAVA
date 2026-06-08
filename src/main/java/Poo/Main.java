8ec27f71bc6fa679b5c6cad7f1a040aa4cc29660
Yuri Coene <yuri01coene@gmail.com>
2026-06-08 08:52:32 -0400

    5 questão herança

--- src/main/java/Poo/Main.java
+++ src/main/java/Poo/Main.java
@@ -1,14 +1,7 @@
 package Poo;
 
-import java.math.BigDecimal;
-
 public class Main {
-    public static void main(String[] args){
-        BigDecimal preco = new BigDecimal("49.90");
-        BigDecimal qtd = new BigDecimal("3");
-        BigDecimal desconto = new BigDecimal("0.95");
-        BigDecimal total = preco.multiply(qtd).multiply(desconto);
-        System.out.println("Total: R$ " + total);
+
     }
-}
+
 
---
+++ src/main/java/Poo/Veiculo.java
@@ -0,0 +1,16 @@
+package Poo;
+
+public class Veiculo {
+    public class Veiculo {
+        private String marca;
+        private int ano;
+        public Veiculo(String marca, int ano) {
+            this.marca = marca;
+            this.ano = ano;
+        }
+        public void ligar() {
+            System.out.println(marca + " ligado!");
+        }
+    }
+
+}
