9402ac1fabe26386ff5318f2468b50909d9d80ce
Yuri Coene <yuri01coene@gmail.com>
2026-06-08 08:20:48 -0400

    Q2° questão Wrappers NullPointerException

--- src/main/java/Poo/Main.java
+++ src/main/java/Poo/Main.java
@@ -1,11 +1,9 @@
 package Poo;
 public class Main {
     public static void main(String[] args){
-        int nota = 8;
-        Integer notaObj = nota;
-        System.out.println(nota);
-        System.out.println(notaObj);
-        System.out.println(notaObj.getClass().getSimpleName());
+        Integer X = null;
+        System.out.println(X);
+        System.out.println(X.getClass().getSimpleName());
     }
 }
 
