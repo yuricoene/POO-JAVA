5d4334bb36fb62855605206783998641182ae34c
Yuri Coene <yuri01coene@gmail.com>
2026-06-08 08:16:31 -0400

    Questão 1

---
+++ src/main/java/Poo/Main.java
@@ -0,0 +1,11 @@
+package Poo;
+public class Main {
+    public static void main(String[] args){
+        int nota = 8;
+        Integer notaObj = nota;
+        System.out.println(nota);
+        System.out.println(notaObj);
+        System.out.println(notaObj.getClass().getSimpleName());
+    }
+}
+
