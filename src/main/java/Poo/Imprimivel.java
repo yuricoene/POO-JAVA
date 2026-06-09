f2f51a43e46ae727132a447b7e31bdfbc1f24cc6
Yuri Coene <yuri01coene@gmail.com>
2026-06-08 21:17:54 -0400

    13 questão interface

--- src/main/java/Poo/Imprimivel.java
+++
@@ -1,9 +0,0 @@
-package Poo;
-
-public abstract class Conta {
-    protected double saldo;
-    public void depositar(double valor) { saldo += valor; }
-    public abstract void sacar(double valor);
-    public double getSaldo() { return saldo; }
-}
-
--- src/main/java/Poo/Relatorio.java
+++
@@ -1,8 +0,0 @@
-package Poo;
-
-public class ContaCorrente extends Imprimivel {
-    @Override
-    public void sacar(double valor) {
-        saldo -= valor; // permite negativo
-    }
-}
