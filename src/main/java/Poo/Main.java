b9d1294572600c6e17f488e280fc214578321446
Yuri Coene <yuri01coene@gmail.com>
2026-06-08 21:24:31 -0400

    16 questão polimorfismo

--- src/main/java/Poo/Main.java
+++ src/main/java/Poo/Main.java
@@ -1,4 +1,14 @@
 package Poo;
 
 public class Main {
+    List animais = new ArrayList<>();
+
+    animais.add(new Cachorro("Rex"));
+    animais.add(new Gato("Mimi"));
+    animais.add(new Cachorro("Thor"));
+    animais.add(new Gato("Luna"));
+    for (Animal a : animais) {
+        a.emitirSom();
+    }
 }
+
